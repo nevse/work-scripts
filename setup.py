f9feb25a9a9ef01d173550631cee69379289724f
Eugene Novikov <eugene.novikov@devexpress.com>
2021-12-17 22:44:43 +0300

    wrap in pip package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="devtools-pkg",
+    version="0.0.1",
+    author="EN",
+    author_email="nevse@gmail.com",
+    description="devtools-pkg",
+    url="https://github.com/nevse/work-scripts",
+    project_urls={
+        "Bug Tracker": "https://github.com/nevse/work-scripts/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="scripts"),
+    python_requires=">=3.6",
+    scripts=["scripts/conv", "scripts/conv.py"]
+)
